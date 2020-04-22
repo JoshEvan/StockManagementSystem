a76ef83d277aa9713762797bb0ef85e0a3abecd0
Joshua Evan Arijanto <joshua.arijanto@gmail.com>
2020-04-22 21:37:51 +0700

    a

---
+++ src/test/java/com/joshua/StockManagementSystem/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.StockManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
