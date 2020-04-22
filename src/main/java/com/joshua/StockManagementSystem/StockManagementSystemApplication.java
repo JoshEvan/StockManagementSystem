3b5fe620f0b195ab8d5775273b98c5653dbab754
Joshua Evan Arijanto <45915433+JoshEvan@users.noreply.github.com>
2020-04-22 20:56:51 +0700

    Add files via upload

---
+++ src/main/java/com/joshua/StockManagementSystem/StockManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.joshua.StockManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementSystemApplication.class, args);
+	}
+
+}
