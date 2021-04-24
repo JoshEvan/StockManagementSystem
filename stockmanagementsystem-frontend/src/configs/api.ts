5ed30d030ba12af81ab3db4bb4080f7516a4ef0a
Joshua Evan Arijanto <joshua.arijanto@gmail.com>
2021-04-24 14:42:02 +0700

    update jar name

--- stockmanagementsystem-frontend/src/configs/api.ts
+++ stockmanagementsystem-frontend/src/configs/api.ts
@@ -4,7 +4,7 @@ const API_URL = {
         BASE_URL: 'http://localhost:8080/api/v1/joseph'
     },
     PROD: {
-        BASE_URL:'https://joseph-sms.herokuapp.com/api/v1/joseph'
+        BASE_URL: process.env.BACKEND_URL
     }
 }
 
