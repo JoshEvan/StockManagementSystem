1c0d23b426476021fd4e9534dc2560e63ca1a6d8
Joshua Evan Arijanto <joshua.arijanto@gmail.com>
2021-04-24 13:03:25 +0700

    cleaning repo

--- src/main/resources/db/migration/V3__InitializePayments.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE if not exists payments(
-    id varchar PRIMARY KEY NOT NULL,
-    payment_type VARCHAR,
-    timestamp timestamp default current_timestamp,
-    is_active boolean DEFAULT TRUE
-);
--- src/main/resources/db/migration/V4__InitializeProductions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE if not exists productions(
-    id varchar NOT NULL PRIMARY KEY,
-    item_code varchar REFERENCES items(item_code),
-    production_date DATE,
-    producer varchar,
-    quantity int,
-    timestamp timestamp default current_timestamp,
-    is_active boolean DEFAULT TRUE
-);
